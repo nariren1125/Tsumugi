be7915a1f5a3d7142ae52a281d6283c940dc64a2
成松廉 <ren1125.9872@gmail.com>
2025-11-14 16:26:34 +0900

    ユーザーモデルの制約追加（line_uid unique / family_group_id null許可）

--- db/migrate/20251110021340_create_users.rb
+++ db/migrate/20251110021340_create_users.rb
@@ -4,9 +4,11 @@ class CreateUsers < ActiveRecord::Migration[7.2]
       t.string :name
       t.string :line_uid
       t.string :email
-      t.references :family_group, null: false, foreign_key: true
+      t.references :family_group, null: true, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :users, :line_uid, unique: true
   end
 end
