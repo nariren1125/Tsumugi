977cfeb44d827fd24535aaf9ccee273b3e36444e
成松廉 <ren1125.9872@gmail.com>
2025-11-20 15:59:04 +0900

    テストコードの修正

--- test/controllers/albums_controller_test.rb
+++ test/controllers/albums_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class AlbumsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create!(name: 'Test User', line_uid: 'test-uid')
+    @album = albums(:one)
+  end
+
   test 'should get index' do
+    # ログイン状態を作る
+    begin
+      post login_path, params: { line_uid: @user.line_uid }
+    rescue StandardError
+      nil
+    end
+    # もしくは session を強制的に書き換える
+    @controller.session[:user_id] = @user.id
+
     get albums_index_url
     assert_response :success
   end
