6b9285fa8f0edc8f0d8e367f7419e103da0fe8be
成松廉 <ren1125.9872@gmail.com>
2025-11-22 22:18:34 +0900

    FlashメッセージとdaisyUI導入によるUI改善

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+    const flashes = document.querySelectorAll(".flash");
+  
+    flashes.forEach((flash) => {
+      setTimeout(() => {
+        flash.classList.add("opacity-0"); // フェードアウト
+      }, 3000);
+  
+      setTimeout(() => {
+        flash.remove(); // DOMから削除
+      }, 3500);
+    });
+});
