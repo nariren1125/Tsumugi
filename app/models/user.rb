f8815a8860397ce241be521314436f3c8afeb747
成松廉 <ren1125.9872@gmail.com>
2025-11-27 00:54:31 +0900

    投稿画面を追加しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # family_groupが無いユーザーも存在する仕様
   belongs_to :family_group, optional: true
+  has_many :posts, dependent: :destroy
 
   validates :line_uid, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 50 }
