4ad5326679b0692fb1564bd233e3615c8e352e59
成松廉 <ren1125.9872@gmail.com>
2025-11-29 20:02:09 +0900

    お子さま登録機能を実装

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,5 +1,35 @@
 class ChildrenController < ApplicationController
+  before_action :require_login
   def new; end
 
-  def create; end
+  def create
+    return redirect_no_family unless current_user.family_group
+
+    child = build_child
+
+    child.save ? redirect_success : redirect_failure(child)
+  end
+
+  private
+
+  def build_child
+    current_user.family_group.children.new(child_params)
+  end
+
+  def redirect_no_family
+    redirect_to family_settings_path, alert: t('children.no_family')
+  end
+
+  def redirect_success
+    redirect_to family_settings_path, notice: t('children.created')
+  end
+
+  def redirect_failure(child)
+    flash[:errors] = child.errors.full_messages
+    redirect_to family_settings_path
+  end
+
+  def child_params
+    params.require(:child).permit(:name, :birth_date)
+  end
 end
--- test/controllers/children_controller_test.rb
+++ test/controllers/children_controller_test.rb
@@ -1,13 +1,28 @@
 require 'test_helper'
 
 class ChildrenControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    @family_group = family_groups(:one)
+
+    log_in_as(@user)
+  end
+
   test 'should get new' do
-    get children_new_url
+    get new_child_url
     assert_response :success
   end
 
-  test 'should get create' do
-    get children_create_url
-    assert_response :success
+  test 'should create child' do
+    assert_difference('Child.count', 1) do
+      post children_url, params: {
+        child: {
+          name: 'テスト太郎',
+          birth_date: '2020-01-01'
+        }
+      }
+    end
+
+    assert_redirected_to family_settings_url
   end
 end
