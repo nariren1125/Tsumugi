14b2a75fefb20ae252c1d6e03ccabae3b82150a4
成松廉 <ren1125.9872@gmail.com>
2025-12-03 23:08:56 +0900

    UIの調整

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,7 +2,20 @@ class AlbumsController < ApplicationController
   before_action :require_login
 
   def index
-    @photos = Photo.all
+    family = current_user.family_group
+
+    # ガード（超重要）
+    unless family
+      @photos_by_year = {}
+      return
+    end
+
+    photos = family.posts
+                   .includes(:photos)
+                   .flat_map(&:photos)
+
+    @photos_by_year = photos.group_by { |photo| photo.created_at.year }
+                            .sort.reverse.to_h
   end
 
   def require_login
