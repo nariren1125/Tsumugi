fcc967be6cf0892e137ba30567347e1e825e3864
成松廉 <ren1125.9872@gmail.com>
2025-12-11 11:07:34 +0900

    家族招待機能の改善

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,16 @@ class SessionsController < ApplicationController
     redirect_to root_path, notice: t('flash.logout.success')
   end
 
+  def build_user_from_auth(auth)
+    # --- ログ出力（LINEから渡された情報を確認） ---
+    log_auth_info(auth)
+    # --- LINE UID を元にユーザーを新規作成 or 取得 ---
+    user = find_or_create_user(auth)
+    # --- 招待リンク経由でアクセスしている場合、家族グループに自動参加させる ---
+    assign_user_to_invited_group(user)
+    user
+  end
+
   # 開発環境用の簡易ログイン
   def dev_login
     user = User.find(params[:id])
@@ -26,15 +36,6 @@ class SessionsController < ApplicationController
     login_success
   rescue StandardError => e
     login_failure(e)
-  end
-
-  # --- User作成まで ---
-  def build_user_from_auth(auth)
-    log_auth_info(auth)
-    User.find_or_create_by!(line_uid: auth['uid']) do |u|
-      u.name = auth.dig('info', 'name')
-      u.email = nil
-    end
   end
 
   # --- ログインセッション管理 ---
@@ -58,5 +59,21 @@ class SessionsController < ApplicationController
     Rails.logger.info "LINE UID: #{auth['uid']}"
     Rails.logger.info "NAME: #{auth.dig('info', 'name')}"
     Rails.logger.info "IMAGE: #{auth.dig('info', 'image')}"
+  end
+
+  def find_or_create_user(auth)
+    User.find_or_create_by!(line_uid: auth['uid']) do |u|
+      u.name  = auth.dig('info', 'name')
+      u.email = nil
+    end
+  end
+
+  def assign_user_to_invited_group(user)
+    invite_group_id = session[:invite_family_group_id]
+    return unless invite_group_id.present? && user.family_group.nil?
+
+    user.update!(family_group_id: invite_group_id)
+    Rails.logger.info "ユーザー#{user.id}をグループ#{invite_group_id}に追加しました"
+    session.delete(:invite_family_group_id)
   end
 end
