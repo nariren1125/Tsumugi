107343b426e5efc162a4253e64259154bff0c0b1
成松廉 <ren1125.9872@gmail.com>
2025-12-17 16:11:56 +0900

    routengを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get "sessions/destroy"
   get "posts/new", to: "posts#new", as: :new_post
   get "invite/:token", to: "invite_tokens#show", as: :invite
+  get "/terms",   to: "pages#terms"
+  get "/privacy", to: "pages#privacy"
   root "home#index"
 
   resources :albums, only: [:index, :show, :new, :create]
