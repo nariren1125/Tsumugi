d02e2df75fb24c6021ad2400a41a21d58aeb2b4a
成松廉 <ren1125.9872@gmail.com>
2025-12-17 16:26:07 +0900

    rubocopの修正

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get terms" do
+  test 'should get terms' do
     get pages_terms_url
     assert_response :success
   end
 
-  test "should get privacy" do
+  test 'should get privacy' do
     get pages_privacy_url
     assert_response :success
   end
