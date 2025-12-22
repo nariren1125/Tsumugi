10bef212df07231d56c3f7f98e35f38e2a438fcd
成松廉 <ren1125.9872@gmail.com>
2025-12-22 13:16:34 +0900

    rubocopの修正

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, inverse_of: :photos
   # imagesがActive Storageで管理されるように設定
   has_one_attached :image
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   belongs_to :child, optional: true
 
   # 写真との関連付け（並び順を持たせる）
-  has_many :photos, -> { order(:position) }, dependent: :destroy
+  has_many :photos, -> { order(:position) }, dependent: :destroy, inverse_of: :post
   # PostモデルがPhotoモデルの属性を受け入れるように設定
   accepts_nested_attributes_for :photos, allow_destroy: true
 
