6d99da754b67c0184f22aea14ecbda7973d65db6
成松廉 <ren1125.9872@gmail.com>
2025-12-26 19:00:57 +0900

    役割を日本語表記に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,23 @@ class User < ApplicationRecord
   def role_i18n
     I18n.t("activerecord.attributes.user.roles.#{role}")
   end
+
+  # enumの選択肢を日本語で返す
+  def self.role_options
+    roles.keys.map do |key|
+      [I18n.t("activerecord.attributes.user.role.#{key}"), key]
+    end
+  end
+
+  # roleに応じたバッジのクラスを返す
+  def role_badge_class
+    case role
+    when "father"
+      "border-blue-400 text-blue-600 bg-blue-50"
+    when "mother"
+      "border-pink-400 text-pink-600 bg-pink-50"
+    else
+      "border-base-300 text-base-content bg-base-100"
+    end
+  end
 end
