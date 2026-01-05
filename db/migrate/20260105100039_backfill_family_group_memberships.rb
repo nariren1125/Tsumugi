e4fe3c2cb590a23382e850ea7c275dc123c78f07
成松廉 <ren1125.9872@gmail.com>
2026-01-06 04:58:34 +0900

    roleカラムがまだ無くてもOKにするための修正

--- db/migrate/20260105100039_backfill_family_group_memberships.rb
+++ db/migrate/20260105100039_backfill_family_group_memberships.rb
@@ -1,12 +1,13 @@
 class BackfillFamilyGroupMemberships < ActiveRecord::Migration[7.2]
   def up
     say_with_time "Backfilling family_group_memberships from users.family_group_id" do
-      User.where.not(family_group_id: nil).find_each do |user|
-        FamilyGroupMembership.find_or_create_by!(
-          user_id: user.id,
-          family_group_id: user.family_group_id
-        )
-      end
+      execute <<~SQL
+        INSERT INTO family_group_memberships (user_id, family_group_id, created_at, updated_at)
+        SELECT id, family_group_id, NOW(), NOW()
+        FROM users
+        WHERE family_group_id IS NOT NULL
+        ON CONFLICT (user_id, family_group_id) DO NOTHING;
+      SQL
     end
   end
 
