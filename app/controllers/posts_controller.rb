9262cfb767b6eb67c0bf044f301d1117ae0513a0
成松廉 <ren1125.9872@gmail.com>
2026-01-16 16:12:37 +0900

    rubocopの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,31 @@
+# ================================
+# コントローラー: 投稿関連
+# ================================
 class PostsController < ApplicationController
+  # 認証と前処理
   before_action :require_login
   before_action :set_post, only: %i[edit update destroy]
   before_action :authorize_user!, only: %i[edit update destroy]
-
   before_action :set_person_context, only: %i[new edit]
 
+  # 新規投稿画面
   def new
-    @post =
-      if params[:draft_post_id].present?
-        current_user.posts.find(params[:draft_post_id])
-      else
-        Post.new
-      end
+    @post = if params[:draft_post_id].present?
+              current_user.posts.find(params[:draft_post_id])
+            else
+              Post.new
+            end
   end
 
+  # 編集画面
   def edit; end
 
+  # 投稿作成処理
   def create
     Rails.logger.debug { "POST PARAMS: #{post_params}" }
 
     if @post.update(post_params)
       notify_family_group_members(@post)
-
       redirect_to albums_path, notice: t('flash.posts.created')
     else
       set_person_context
@@ -29,6 +33,7 @@ class PostsController < ApplicationController
     end
   end
 
+  # 投稿更新処理
   def update
     if @post.update(post_params)
       redirect_to albums_path, notice: t('flash.posts.updated')
@@ -38,24 +43,22 @@ class PostsController < ApplicationController
     end
   end
 
+  # 投稿削除処理
   def destroy
     @post.destroy
     redirect_to albums_path, notice: t('flash.posts.deleted')
   end
 
+  # 写真選択画面
   def select_photos; end
 
-  #  写真を仮保存し、確認プレビューを表示する
+  # 写真確認（仮保存）
   def confirm_photos
     images = params.dig(:post, :images)
     return redirect_to_no_photos if images.blank?
 
     max = 5
-
-    # サーバー側で必ず制限する（最重要）
     limited_images = images.first(max)
-
-    # 画面表示用（何枚切り捨てたか）
     @dropped_files_count = [images.size - max, 0].max
 
     @post = build_draft_post
@@ -64,47 +67,31 @@ class PostsController < ApplicationController
 
   private
 
-  # ==================================================
-  # Strong Parameters
-  # ==================================================
+  # ストロングパラメータ
   def post_params
-    params.require(:post).permit(
-      :title,
-      :content,
-      :child_id,
-      :photo_date,
-      person_tag_ids: []
-    )
+    params.require(:post).permit(:title, :content, :child_id, :photo_date, person_tag_ids: [])
   end
 
-  # ==================================================
-  # Confirm Photos Flow
-  # ==================================================
-
+  # 写真未選択リダイレクト
   def redirect_to_no_photos
-    redirect_to select_photos_posts_path,
-                alert: t('flash.posts.no_photos_selected')
+    redirect_to select_photos_posts_path, alert: t('flash.posts.no_photos_selected')
   end
 
+  # 仮投稿生成
   def build_draft_post
     post = current_user.posts.build(album: temp_album)
     post.save(validate: false)
     post
   end
 
+  # 写真を添付
   def attach_photos(post, images)
     images.each_with_index do |img, index|
-      post.photos.create!(
-        image: img,
-        position: index
-      )
+      post.photos.create!(image: img, position: index)
     end
   end
 
-  # ==================================================
-  # Album
-  # ==================================================
-
+  # 一時アルバム取得
   def temp_album
     family = current_family_group
     raise ActiveRecord::RecordNotFound, 'family_group not selected' unless family
@@ -112,46 +99,47 @@ class PostsController < ApplicationController
     family.albums.first || family.albums.create!(title: '思い出アルバム')
   end
 
-  # ==================================================
-  # Before Actions
-  # ==================================================
-
+  # 投稿取得
   def set_post
     @post = current_user.posts.find(params[:id])
   end
 
+  # 投稿権限確認
   def authorize_user!
     return if @post.user == current_user
 
     redirect_to albums_path, alert: t('flash.authorization.failed')
   end
 
-  # 人物タグの共通セット処理
+  # 人物タグのセット
   def set_person_context
     family_group = current_family_group
     @has_family_group = family_group.present?
-    @person_tags =
-      if family_group
-        PersonTag.where(family_group_id: family_group.id).order(:name)
-      else
-        []
-      end
+    @person_tags = family_group ? PersonTag.where(family_group_id: family_group.id).order(:name) : []
   end
 
   # LINE通知を家族グループのメンバーに送信
   def notify_family_group_members(post)
     family_group = post.album.family_group
     return unless family_group
-  
+
     notifier = LineNotifier.new
-    family_group.users.each do |member|
-      next if member.id == current_user.id
-      next if member.line_uid.blank?
-  
-      notifier.push_message(
-        member.line_uid,
-        "#{current_user.name}さんが新しい思い出を投稿しました📸\n\n#{post.title.presence || 'タイトルなし'}"
-      )
+    recipient_uids = line_uids_to_notify(family_group.users)
+    message = build_post_message(post)
+
+    recipient_uids.each do |uid|
+      notifier.push_message(uid, message)
     end
+  end
+
+  # 通知対象のLINE UIDリストを抽出（自分以外でUIDが存在するユーザー）
+  def line_uids_to_notify(users)
+    users.reject { |u| u.id == current_user.id || u.line_uid.blank? }.map(&:line_uid)
+  end
+
+  # 投稿に関する通知メッセージを生成
+  def build_post_message(post)
+    title = post.title.presence || 'タイトルなし'
+    "#{current_user.name}さんが新しい思い出を投稿しました📸\n\n#{title}"
   end
 end
--- app/services/line_notifier.rb
+++ app/services/line_notifier.rb
@@ -1,32 +1,43 @@
-require 'net/http'
-require 'uri'
-require 'json'
-
+# ========================================
+# LINE通知用のサービスクラス
+# ========================================
 class LineNotifier
-  LINE_PUSH_API = 'https://api.line.me/v2/bot/message/push'
+  require 'net/http'
+  require 'uri'
+  require 'json'
 
-  def initialize
-    @token = Rails.application.credentials.dig(:line, :access_token)
-  end
-
+  # 指定したLINEユーザーにメッセージを送信する
   def push_message(to, message)
-    uri = URI.parse(LINE_PUSH_API)
-    header = {
-      "Content-Type" => "application/json",
-      "Authorization" => "Bearer #{@token}"
-    }
-
-    body = {
-      to: to,
-      messages: [{ type: 'text', text: message }]
-    }
-
+    uri = URI.parse('https://api.line.me/v2/bot/message/push')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    request = Net::HTTP::Post.new(uri.request_uri, header)
-    request.body = body.to_json
-    response = http.request(request)
 
-    Rails.logger.info "LINE通知：#{response.code} #{response.body}"
+    request = Net::HTTP::Post.new(uri.request_uri, headers)
+    request.body = build_payload(to, message).to_json
+
+    http.request(request)
+  end
+
+  private
+
+  # LINE Messaging API に必要なリクエストヘッダーを定義
+  def headers
+    {
+      'Content-Type' => 'application/json',
+      'Authorization' => "Bearer #{ENV.fetch('LINE_CHANNEL_ACCESS_TOKEN', nil)}"
+    }
+  end
+
+  # LINE APIに送るためのJSON形式のリクエストボディを構築
+  def build_payload(to, message)
+    {
+      to: to,
+      messages: [
+        {
+          type: 'text',
+          text: message
+        }
+      ]
+    }
   end
 end
