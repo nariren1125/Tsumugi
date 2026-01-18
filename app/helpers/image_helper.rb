500f0c6ff7b93b1fbdb36bd10d15a7923350164e
成松廉 <ren1125.9872@gmail.com>
2026-01-18 19:11:38 +0900

    ActiveStorage標準の表示方式へ変更

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,12 +1,2 @@
 module ImageHelper
-  def cdn_image_path(blob, variant: nil)
-    return '' unless blob.attached?
-
-    if Rails.env.production?
-      key = variant ? blob.variant(variant).processed.key : blob.key
-      "https://d47gzlc2fllgd.cloudfront.net/rails/active_storage/blobs/#{key}"
-    else
-      url_for(variant ? blob.variant(variant) : blob)
-    end
-  end
 end
