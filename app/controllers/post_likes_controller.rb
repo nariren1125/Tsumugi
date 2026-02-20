50659f2b43f39381d10c442ca6dfc1e236525fd6
成松廉 <ren1125.9872@gmail.com>
2026-02-20 14:12:18 +0900

    rubocopの修正

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -1,6 +1,5 @@
 # app/controllers/post_likes_controller.rb
 class PostLikesController < ApplicationController
-
   def index
     # 変更前: @posts = Post.all など
     # 変更後: includes を追加
@@ -10,10 +9,10 @@ class PostLikesController < ApplicationController
   # いいねを作成するアクション
   def create
     @post = Post.find(params[:post_id])
-    
+
     # 変更: すでにある場合は作成せず、無い場合だけ作成する
     @post_like = current_user.post_likes.find_or_create_by(post_id: @post.id)
-    
+
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_back(fallback_location: root_path) }
@@ -24,7 +23,7 @@ class PostLikesController < ApplicationController
   def destroy
     @post = Post.find(params[:post_id])
     @post_like = current_user.post_likes.find_by(post_id: @post.id)
-    @post_like.destroy if @post_like
+    @post_like&.destroy
 
     respond_to do |format|
       format.turbo_stream
