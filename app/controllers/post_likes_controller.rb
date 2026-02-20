515319aa89cc911a54c6604d66fc71e53845ef5e
成松廉 <ren1125.9872@gmail.com>
2026-02-20 14:01:28 +0900

    コメントシートおよび思い出のUI/UXの改善

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -1,32 +1,34 @@
 # app/controllers/post_likes_controller.rb
 class PostLikesController < ApplicationController
+
+  def index
+    # 変更前: @posts = Post.all など
+    # 変更後: includes を追加
+    @posts = Post.includes(:post_likes, :comments).order(created_at: :desc)
+  end
+
   # いいねを作成するアクション
   def create
     @post = Post.find(params[:post_id])
     
-    # current_userに紐づく形で、この投稿へのいいねを作成
-    @post_like = current_user.post_likes.new(post_id: @post.id)
-
-    if @post_like.save
-      # 成功したら元の画面（投稿一覧など）に戻る
-      redirect_back(fallback_location: root_path)
-    else
-      # 失敗時（すでにいいねしている場合など）も元の画面に戻る
-      redirect_back(fallback_location: root_path, alert: 'いいねできませんでした')
+    # 変更: すでにある場合は作成せず、無い場合だけ作成する
+    @post_like = current_user.post_likes.find_or_create_by(post_id: @post.id)
+    
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back(fallback_location: root_path) }
     end
   end
 
   # いいねを解除するアクション
   def destroy
     @post = Post.find(params[:post_id])
-    
-    # current_userがこの投稿にしたいいねを探す
     @post_like = current_user.post_likes.find_by(post_id: @post.id)
-    
-    # 存在すれば削除する
     @post_like.destroy if @post_like
 
-    # 元の画面に戻る
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back(fallback_location: root_path) }
+    end
   end
 end
